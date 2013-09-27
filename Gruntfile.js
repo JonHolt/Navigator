f1983532c63d13a0735147210690914994fc1c26
MCluck <mcluck90@gmail.com>
2013-09-26 19:02:30 -0600

    Setup basic Gruntfile and build step for Psykick

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = function(grunt) {
+    // Load in all the grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    var PSYKICK_FILES = [
+        'core',
+        'keys',
+        'helper',
+        'component',
+        'entity',
+        'layer',
+        'system',
+        '*-system',
+        'physics',
+        'components/**/*',
+        'systems/**/*'
+    ];
+
+    // Make sure all of the Psykick files come from the correct folder
+    for (var i = 0, len = PSYKICK_FILES.length; i < len; i++) {
+        PSYKICK_FILES[i] = 'client/psykick/js/engine/' + PSYKICK_FILES[i] + '.js';
+    }
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                ignores: ['**/node_modules/', 'client/**/*.min.js']
+            },
+            all: {
+                files: {
+                    src: ['client/js/**/*.js', 'server/**/*.js']
+                }
+            }
+        },
+
+        uglify: {
+            options: {
+                mangle: false
+            },
+            psykick: {
+                files: {
+                    'client/js/psykick.min.js': PSYKICK_FILES
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('build', ['uglify:psykick']);
+};
